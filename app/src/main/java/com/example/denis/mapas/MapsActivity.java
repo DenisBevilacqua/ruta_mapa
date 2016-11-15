efc749e035e2d9bd8eb1ac87080cf8f5bfbcdf59
Denis <denisbevilacqua6@gmail.com>
2016-11-15 00:04:03 -0300

    Se agrega ubicación actual.

--- app/src/main/java/com/example/denis/mapas/MapsActivity.java
+++ app/src/main/java/com/example/denis/mapas/MapsActivity.java
@@ -1,8 +1,14 @@
 package com.example.denis.mapas;
 
+import android.*;
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -12,6 +18,8 @@ import com.akexorcist.googledirection.GoogleDirection;
 import com.akexorcist.googledirection.constant.TransportMode;
 import com.akexorcist.googledirection.model.Direction;
 import com.akexorcist.googledirection.util.DirectionConverter;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.drive.Drive;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -21,6 +29,8 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.ArrayList;
 
+import im.delight.android.location.SimpleLocation;
+
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback, View.OnClickListener, DirectionCallback {
     private Button btnRequestDirection;
     private GoogleMap googleMap;
@@ -29,6 +39,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     private LatLng origin = new LatLng(-31.649846, -60.717204);
     private LatLng destination = new LatLng(-31.616424, -60.675253);
     private boolean mostrandoMapa = false;
+    private SimpleLocation location;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +50,31 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         btnRequestDirection.setOnClickListener(this);
 
         ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMapAsync(this);
+
+        // construct a new instance of SimpleLocation
+        location = new SimpleLocation(this);
+
+        // if we can't access the location yet
+        if (!location.hasLocationEnabled()) {
+            // ask the user to enable location access
+            SimpleLocation.openSettings(this);
+        }
+
+        findViewById(R.id.botonUbicacionActual).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                final double latitude = location.getLatitude();
+                final double longitude = location.getLongitude();
+                LatLng latLng = new LatLng(latitude,longitude);
+
+
+                googleMap.addMarker(new MarkerOptions().position(latLng).title("Mi ubicación"));
+                // TODO
+            }
+
+        });
+
     }
 
     @Override
@@ -80,17 +116,65 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
             mostrandoMapa = true;
             btnRequestDirection.setText("Limpiar mapa");
             btnRequestDirection.setVisibility(View.VISIBLE);
-            googleMap.addMarker(new MarkerOptions().position(origin));
-            googleMap.addMarker(new MarkerOptions().position(destination));
+            googleMap.addMarker(new MarkerOptions().position(origin).title("Origen"));
+            googleMap.addMarker(new MarkerOptions().position(destination).title("Destino"));
 
             ArrayList<LatLng> directionPositionList = direction.getRouteList().get(0).getLegList().get(0).getDirectionPoint();
-            googleMap.addPolyline(DirectionConverter.createPolyline(this, directionPositionList, 7, Color.BLUE));
+            googleMap.addPolyline(DirectionConverter.createPolyline(this, directionPositionList, 7, Color.parseColor("#2196F3")));
 
         }
     }
 
     @Override
     public void onDirectionFailure(Throwable t) {
-        Snackbar.make(btnRequestDirection, t.getMessage(), Snackbar.LENGTH_SHORT).show();
+        Snackbar.make(btnRequestDirection, "No hemos podido mostrarte la ruta: " + t.getMessage(), Snackbar.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // make the device update its location
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                // Show an expanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        0);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
+
+
+        //location.beginUpdates();
+
+        // ...
+    }
+
+    @Override
+    protected void onPause() {
+        // stop location updates (saves battery)
+        location.endUpdates();
+
+        // ...
+
+        super.onPause();
     }
 }
