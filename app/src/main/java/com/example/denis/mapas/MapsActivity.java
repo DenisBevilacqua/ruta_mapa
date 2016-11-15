4d54b4e57945b4151ea991aac07efba72710dac3
Denis <denisbevilacqua6@gmail.com>
2016-11-15 11:52:06 -0300

    Se agrega ubicación actual. Se agrega ruta desde ubicación actual hasta el origen del trayecto que se quiere realizar.

--- app/src/main/java/com/example/denis/mapas/MapsActivity.java
+++ app/src/main/java/com/example/denis/mapas/MapsActivity.java
@@ -10,8 +10,13 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.akexorcist.googledirection.DirectionCallback;
 import com.akexorcist.googledirection.GoogleDirection;
@@ -20,11 +25,13 @@ import com.akexorcist.googledirection.model.Direction;
 import com.akexorcist.googledirection.util.DirectionConverter;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.drive.Drive;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.ArrayList;
@@ -35,9 +42,14 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     private Button btnRequestDirection;
     private GoogleMap googleMap;
     private String serverKey = "AIzaSyAkVcdTXj47qfUPWnDKNK_d2luTpcx1PmM";
-    private LatLng camera = new LatLng(-31.649846, -60.717204);
-    private LatLng origin = new LatLng(-31.649846, -60.717204);
-    private LatLng destination = new LatLng(-31.616424, -60.675253);
+    private LatLng camera = new LatLng(-31.635468, -60.701156);
+    private LatLng originRoute = new LatLng(-31.635468, -60.701156);
+    private LatLng destinationRoute = new LatLng(-31.616424, -60.675253);
+    private LatLng miUbicacion;
+    private LatLng origenActual;
+    private LatLng destinoActual;
+    private Integer color;
+
     private boolean mostrandoMapa = false;
     private SimpleLocation location;
 
@@ -45,6 +57,8 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_simple_direction);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         btnRequestDirection = (Button) findViewById(R.id.btn_request_direction);
         btnRequestDirection.setOnClickListener(this);
@@ -64,17 +78,51 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
             @Override
             public void onClick(View v) {
+
+                location.beginUpdates();
+                
+
                 final double latitude = location.getLatitude();
                 final double longitude = location.getLongitude();
-                LatLng latLng = new LatLng(latitude,longitude);
+                miUbicacion = new LatLng(latitude,longitude);
 
+                googleMap.addMarker(new MarkerOptions().position(miUbicacion).title("Mi ubicación"));
 
-                googleMap.addMarker(new MarkerOptions().position(latLng).title("Mi ubicación"));
-                // TODO
+                googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(miUbicacion, 14));
+
+                color = Color.parseColor("#DF7401");
+                requestDirection(miUbicacion, originRoute, color);
+
             }
 
         });
 
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.limpiar) {
+
+            googleMap.clear();
+            btnRequestDirection.setText("Mostrar ruta");
+            mostrandoMapa = false;
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -87,8 +135,10 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void onClick(View v) {
         int id = v.getId();
         if (id == R.id.btn_request_direction) {
-            if (mostrandoMapa == false)
-            requestDirection();
+            if (mostrandoMapa == false) {
+                color = Color.parseColor("#2196F3");
+                requestDirection(originRoute, destinationRoute, color);
+            }
             else
             {
                 googleMap.clear();
@@ -99,28 +149,41 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
     }
 
-    public void requestDirection() {
+    public void requestDirection(LatLng origin, LatLng destination, Integer color) {
         Snackbar.make(btnRequestDirection, "Buscando dirección...", Snackbar.LENGTH_SHORT).show();
         GoogleDirection.withServerKey(serverKey)
                 .from(origin)
                 .to(destination)
                 .transportMode(TransportMode.DRIVING)
                 .execute(this);
+
+        origenActual = origin;
+        destinoActual = destination;
+        this.color = color;
     }
 
     @Override
     public void onDirectionSuccess(Direction direction, String rawBody) {
-        Snackbar.make(btnRequestDirection, "Se ha realizado la operación con el siguiente estado: " + direction.getStatus(), Snackbar.LENGTH_SHORT).show();
+        Snackbar.make(btnRequestDirection, "Se ha realizado la operación con el siguiente estado: " + direction.getStatus(), Snackbar.LENGTH_LONG).show();
         if (direction.isOK()) {
 
             mostrandoMapa = true;
             btnRequestDirection.setText("Limpiar mapa");
             btnRequestDirection.setVisibility(View.VISIBLE);
-            googleMap.addMarker(new MarkerOptions().position(origin).title("Origen"));
-            googleMap.addMarker(new MarkerOptions().position(destination).title("Destino"));
+            googleMap.addMarker(new MarkerOptions().position(origenActual).title("Origen"));
+            googleMap.addMarker(new MarkerOptions().position(destinoActual).title("Destino"));
 
             ArrayList<LatLng> directionPositionList = direction.getRouteList().get(0).getLegList().get(0).getDirectionPoint();
-            googleMap.addPolyline(DirectionConverter.createPolyline(this, directionPositionList, 7, Color.parseColor("#2196F3")));
+            googleMap.addPolyline(DirectionConverter.createPolyline(this, directionPositionList, 7, color ));
+
+            // Calculamos distancia entre ambos puntos
+
+            Double distancia = location.calculateDistance(origenActual.latitude, origenActual.longitude, destinoActual.latitude, destinoActual.longitude);
+
+            distancia = Math.round( distancia * 100.0 ) / 100.0;
+
+            Toast.makeText(getApplicationContext(), "La distancia entre origen y destino es de: " + distancia + " metros",
+                    Toast.LENGTH_LONG).show();
 
         }
     }
@@ -134,46 +197,34 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     protected void onResume() {
         super.onResume();
 
-        // make the device update its location
+        // Pedimos permiso para obtener ubicación.
 
         if (ContextCompat.checkSelfPermission(this,
                 Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
 
-            // Should we show an explanation?
             if (ActivityCompat.shouldShowRequestPermissionRationale(this,
                     Manifest.permission.ACCESS_FINE_LOCATION)) {
 
-                // Show an expanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
 
             } else {
 
-                // No explanation needed, we can request the permission.
 
                 ActivityCompat.requestPermissions(this,
                         new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
                         0);
-
-                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-                // app-defined int constant. The callback method gets the
-                // result of the request.
             }
         }
 
+        // Deberíamos correr este código.
+        // location.beginUpdates();
 
-        //location.beginUpdates();
-
-        // ...
     }
 
     @Override
     protected void onPause() {
         // stop location updates (saves battery)
         location.endUpdates();
-
-        // ...
 
         super.onPause();
     }
